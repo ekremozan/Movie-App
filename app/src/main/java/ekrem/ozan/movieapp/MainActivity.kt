36b60eced1c8e501523647f6d6ec98e492bfbe09
ekremozan <ekremozanuysal@gmail.com>
2021-03-17 23:17:26 +0300

    First Commit

---
+++ app/src/main/java/ekrem/ozan/movieapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ekrem.ozan.movieapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
